3f42268d423ddf82d18f9d15705a437e2147c7cf
bi19110220-gs <bi19110220@gmail.com>
2026-01-16 00:19:26 +0800

    [580] [Time Beats: 62.76%] - LeetPush

--- Count-Student-Number-in-Departments.sql
+++ Count-Student-Number-in-Departments.sql
@@ -1,10 +1,6 @@
-1-- select dept_id,count(student_id)as student_number 
-2-- from Student 
-3-- group by dept_id
-4
-5select d.dept_name,count(s.student_id)as student_number 
-6from Department d
-7left join Student s
-8on d.dept_id = s.dept_id
-9group by d.dept_id
-10
+1select d.dept_name,count(s.student_id)as student_number 
+2from Department d
+3left join Student s
+4on d.dept_id = s.dept_id
+5group by d.dept_id
+6order by student_number desc,d.dept_name 
