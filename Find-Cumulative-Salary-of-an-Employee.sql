e2a444e5290c12aff5d85a13ca8cb7a489bc7df7
bi19110220-gs <bi19110220@gmail.com>
2026-01-15 22:26:41 +0800

    [579] [Time Beats: 25.93%] - LeetPush

--- Find-Cumulative-Salary-of-an-Employee.sql
+++ Find-Cumulative-Salary-of-an-Employee.sql
@@ -1,6 +1,13 @@
 1select 
 2    id,
 3    month,
-4    sum(salary)over(partition by id order by month range between preceding 2 and current row)
+4    sum(salary)over(partition by id order by month range between 2 preceding and current row)
 5as salary
 6from employee
+7where(id,month) not in 
+8(
+9    select id,max(month) as month
+10    from employee
+11    group by id
+12)
+13order by id asc,month desc
